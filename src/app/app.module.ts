b8b154cc2f88c553030d86ab4bf7b76a5585015e
DI-SI-GO <dsifuentesglz@gmail.com>
2018-08-19 15:02:21 -0500

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import {WeatherService} from './weather.service';
 import { AppComponent } from './app.component';
+import { DetailsComponent } from './details/details.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+const appRoutes: Routes = [
+  { path: 'details/:location',      component: DetailsComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  { path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  { path: '**',
+  redirectTo: '/dashboard'
+}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [WeatherService],
   bootstrap: [AppComponent]
+
+
+
+
 })
 export class AppModule { }
